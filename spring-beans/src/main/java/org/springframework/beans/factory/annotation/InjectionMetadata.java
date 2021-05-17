fbe6afa4965c8936e8ab5e744addb63514205e74
zhishengjie <confident_boy_li@163.com>
2021-05-17 17:15:11 +0800

    @Autowire 详解

--- spring-beans/src/main/java/org/springframework/beans/factory/annotation/InjectionMetadata.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/annotation/InjectionMetadata.java
@@ -65,10 +65,14 @@ public class InjectionMetadata {
 
 	public void checkConfigMembers(RootBeanDefinition beanDefinition) {
 		Set<InjectedElement> checkedElements = new LinkedHashSet<>(this.injectedElements.size());
+		// 遍历检查所有要注入的元素
 		for (InjectedElement element : this.injectedElements) {
 			Member member = element.getMember();
+			// 如果beanDefinition的externallyManagedConfigMembers属性不包含该member
 			if (!beanDefinition.isExternallyManagedConfigMember(member)) {
+				// 将该member添加到beanDefinition的externallyManagedConfigMembers属性
 				beanDefinition.registerExternallyManagedConfigMember(member);
+				// 并将element添加到checkedElements
 				checkedElements.add(element);
 				if (logger.isTraceEnabled()) {
 					logger.trace("Registered injected element on class [" + this.targetClass.getName() + "]: " + element);
@@ -80,6 +84,7 @@ public class InjectionMetadata {
 
 	public void inject(Object target, @Nullable String beanName, @Nullable PropertyValues pvs) throws Throwable {
 		Collection<InjectedElement> checkedElements = this.checkedElements;
+		// 如果checkedElements存在，则使用checkedElements，否则使用injectedElements
 		Collection<InjectedElement> elementsToIterate =
 				(checkedElements != null ? checkedElements : this.injectedElements);
 		if (!elementsToIterate.isEmpty()) {
@@ -87,6 +92,8 @@ public class InjectionMetadata {
 				if (logger.isTraceEnabled()) {
 					logger.trace("Processing injected element of bean '" + beanName + "': " + element);
 				}
+				// 解析@Autowired注解生成的元数据类：AutowiredFieldElement、AutowiredMethodElement，
+				// 这两个类继承InjectionMetadata.InjectedElement，各自重写了inject方法。
 				element.inject(target, beanName, pvs);
 			}
 		}
